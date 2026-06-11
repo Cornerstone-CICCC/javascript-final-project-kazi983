e05f1fd47bb09cdaaa91fddb47da4d902c66564d
Kazi <kaoru.kajihara.mail@gmail.com>
2026-06-11 09:43:11 -0700

    complete ex09

--- src/ex09.js
+++ src/ex09.js
@@ -4,9 +4,21 @@
  * @returns {string}
  */
 function capitalizeWords(str) {
+  if (!str && str !== '') throw new Error('String cannot be undefined');
+
+  if (typeof str !== 'string') throw new Error('Argument must be a string');
+
   return str
+    .trim()
     .split(' ')
-    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .filter((word) => word.length > 0)
+    .map((word) =>
+      word.length === 0
+        ? word
+        : word.length === 1
+          ? word[0].toUpperCase()
+          : word[0].toUpperCase() + word.toLowerCase().slice(1),
+    )
     .join(' ');
 }
 
