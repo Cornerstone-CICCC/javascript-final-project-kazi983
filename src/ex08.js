9e9d71e9b5597cda1989cb8c738bff33feed6d00
Kazi <kaoru.kajihara.mail@gmail.com>
2026-06-11 09:37:26 -0700

    complete ex08

--- src/ex08.js
+++ src/ex08.js
@@ -6,7 +6,20 @@
  * @returns {string[]}
  */
 function search(arr, keyword) {
-  return arr.filter((item) => item.includes(keyword));
+  // validate arr
+  if (!arr) throw new Error('Array cannot be undefined');
+
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
+
+  if (arr.some((el) => typeof el !== 'string'))
+    throw new Error('Array can only contain strings');
+
+  // validate keyword
+  if (!keyword && keyword !== '') throw new Error('Keyword cannot be undefined');
+
+  if (typeof keyword !== 'string') throw new Error('Keyword must be a string');
+
+  return [...arr].filter((item) => item.toLowerCase().includes(keyword.toLowerCase()));
 }
 
 module.exports = search;
