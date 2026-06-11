566864a293242266ffb1929df5a0a22806c535aa
Kazi <kaoru.kajihara.mail@gmail.com>
2026-06-11 11:40:30 -0700

    complete ex13

--- src/ex13.js
+++ src/ex13.js
@@ -5,6 +5,18 @@
  * @returns {boolean}
  */
 function checkAnagram(firstString, secondString) {
+  if (!firstString && firstString !== '')
+    throw new Error('First string cannot be undefined');
+
+  if (!secondString && secondString !== '')
+    throw new Error('Second string cannot be undefined');
+
+  if (typeof firstString !== 'string' || typeof secondString !== 'string')
+    throw new Error('Arguments must be strings');
+
+  firstString = firstString.split('').sort().join('');
+  secondString = secondString.split('').sort().join('');
+
   return firstString === secondString;
 }
 
