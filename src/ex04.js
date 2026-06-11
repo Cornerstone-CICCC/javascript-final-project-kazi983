248d28bfc38851129e464a67db8aea2880d490b4
Kazi <kaoru.kajihara.mail@gmail.com>
2026-06-11 09:13:47 -0700

    complete ex04

--- src/ex04.js
+++ src/ex04.js
@@ -4,6 +4,15 @@
  * @returns {number}
  */
 function calculateAverage(arr) {
+  if (!arr) throw new Error('Array cannot be undefined');
+
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
+
+  if (arr.length === 0) throw new Error('Array cannot be empty');
+
+  if (arr.some((el) => typeof el !== 'number'))
+    throw new Error('Array can only contain numbers');
+
   const total = arr.reduce((sum, number) => sum + number, 0);
   return total / arr.length;
 }
