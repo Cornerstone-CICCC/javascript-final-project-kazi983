0b454934e570e1393caf0ba4e7a57753c08e2434
Kazi <kaoru.kajihara.mail@gmail.com>
2026-06-11 09:30:52 -0700

    complete ex07

--- src/ex07.js
+++ src/ex07.js
@@ -4,10 +4,18 @@
  * @returns {{positive: number[], negative: number[], zero: number[]}}
  */
 function classifyNumbers(arr) {
+  if (!arr && (!Array.isArray(arr) || arr.length !== 0))
+    throw new Error('Array cannot be undefined');
+
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
+
+  if (arr.some((el) => typeof el !== 'number'))
+    throw new Error('Array can only contain numbers');
+
   return {
-    positive: arr.filter((number) => number > 0),
-    negative: arr.filter((number) => number < 0),
-    zero: arr.filter((number) => number === 0),
+    positive: [...arr].filter((number) => number > 0),
+    negative: [...arr].filter((number) => number < 0),
+    zero: [...arr].filter((number) => number === 0),
   };
 }
 
