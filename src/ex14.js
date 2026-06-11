b4627c88c91116cf0cc46ebdda43ea848880a2de
Kazi <kaoru.kajihara.mail@gmail.com>
2026-06-11 12:13:57 -0700

    complete ex14

--- src/ex14.js
+++ src/ex14.js
@@ -5,7 +5,36 @@
  * @returns {number[]}
  */
 function moveZeroes(numbers) {
-  return numbers.sort();
+  if (!numbers) throw new Error('Array cannot be undefined');
+
+  if (!Array.isArray(numbers)) throw new Error('Argument must be an array');
+
+  if (numbers.some((el) => typeof el !== 'number'))
+    throw new Error('Array can only contain numbers');
+
+  let res;
+
+  const zeroCount = numbers.filter((num) => num === 0).length;
+  let searchIndex = 0;
+
+  while (numbers.includes(0)) {
+    if (numbers[searchIndex] === 0) {
+      numbers = [
+        ...numbers.slice(0, numbers.findIndex((num) => num === 0, searchIndex)),
+        ...numbers.slice(numbers.findIndex((num) => num === 0, searchIndex) + 1),
+      ];
+    } else {
+      searchIndex ++;
+    }
+  }
+
+  for (let i = 0; i < zeroCount; i++) {
+    numbers.push(0);
+  }
+
+  return numbers;
 }
 
 module.exports = moveZeroes;
+
+const numbers = [0, 1, 0, 3, 12];
