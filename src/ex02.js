7a182e4daf7be5d16371d58807852b87925a31a1
Kazi <kaoru.kajihara.mail@gmail.com>
2026-06-11 09:03:09 -0700

    complete ex02

--- src/ex02.js
+++ src/ex02.js
@@ -4,7 +4,14 @@
  * @returns {number}
  */
 function sumArray(arr) {
-  return arr.reduce((total, number) => total + number);
+  if (!arr) throw new Error('Array cannot be undefined');
+
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
+
+  if (arr.some((el) => typeof el !== 'number'))
+    throw new Error('Array can only contain numbers');
+
+  return arr.reduce((total, number) => total + number, 0);
 }
 
 module.exports = sumArray;
