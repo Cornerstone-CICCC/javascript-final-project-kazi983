84575efa8eb66086dcebcfaf520b479233d7329a
Kazi <kaoru.kajihara.mail@gmail.com>
2026-06-12 09:41:42 -0700

    complete ex17

--- src/ex17.js
+++ src/ex17.js
@@ -6,7 +6,19 @@
  * @returns {number[]}
  */
 function findTwoSum(numbers, target) {
-  if (numbers[0] + numbers[1] === target) return [0, 1];
+  if (!numbers && (!Array.isArray(numbers) || numbers.length !== 0))
+    throw new Error('Array cannot be undefined');
+
+  if (!Array.isArray(numbers)) throw new Error('Argument must be an array');
+
+  if (numbers.some((el) => typeof el !== 'number'))
+    throw new Error('Array can only contain numbers');
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [i, j];
+    }
+  }
 
   return [];
 }
